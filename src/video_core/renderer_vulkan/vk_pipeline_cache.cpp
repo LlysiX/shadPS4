a9ffa7af3134cb19c1c914ea00db30c3f2988992
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-09-23 13:49:26 -0500

    RB4 CHARACTER RENDERING

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -192,7 +192,7 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 // 0xce54e4ddULL - Rock Band 4 V_MOVRELS_B32 - DO NOT USE, makes notes invisible
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 0> skip_hashes = {};
+    static constexpr std::array<u64, 2> skip_hashes = {0xc7f34c4fULL, 0x13a1d5fcULL};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
